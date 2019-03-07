a1f51ce9aea25f03c9d28f9583dcfdc4881c2f46
Rennick Carter M <n01155242@nl-00115-016.mediastudies.ca>
2019-03-07 17:55:09 -0500

    Added ability to get hunger based on date since last fed

--- PetGame/PetGame/PetData.swift
+++ PetGame/PetGame/PetData.swift
@@ -9,16 +9,39 @@ import SpriteKit
 import GameplayKit
 
 class PetData{
-    var lastFed : Date
-    var lastWashed : Date
-    var lastWalked : Date
-    var lastPlayedWith : Date
+    var lastFed : NSDate
+    var lastWashed : NSDate
+    var lastWalked : NSDate
+    var lastPlayedWith : NSDate
+    
+    let hungerDuration : Double = 600.0 //Time to zero hunger in seconds
+    let washDuration : Double = 600.0 //Time to zero clean in seconds
+    let walkDuration : Double = 600.0 //Time to zero tired in seconds
+    let playDuration : Double = 600.0 //Time to zero play in seconds
     
     init(){
-        
+        //Set all our values to the current date and time initially
+        lastFed = NSDate()
+        lastWashed = NSDate()
+        lastWalked = NSDate()
+        lastPlayedWith = NSDate()
     }
     
-    func GetHappiness() -> Float {
+    func GetHappiness() -> Double{
+        //let timeSinceFed = NSDate.timeIntervalSince(lastFed)
+        //let timeSinceWashed = NSDate.timeIntervalSince(lastWashed)
+        //let timeSinceWalked = NSDate.timeIntervalSince(lastWalked)
+        //let timeSincePlayedWith = NSDate.timeIntervalSince(lastPlayedWith)
         return 1.0
+    }
+    
+    func GetHunger() -> Double{
+        let timeSinceFed = lastFed.timeIntervalSinceNow
+        
+        if(timeSinceFed >= hungerDuration){
+            return 0.0;
+        }
+        
+        return 1.0 - (timeSinceFed / hungerDuration)
     }
 }
