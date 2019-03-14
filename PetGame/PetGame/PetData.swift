85bb34d7fdf4fdb17ce2c61ee508ce48b4ae0ff2
Rennick Carter M <n01155242@nl-00115-016.mediastudies.ca>
2019-03-14 17:49:22 -0400

    PetData is now stored using UserDefaults

--- PetGame/PetGame/PetData.swift
+++ PetGame/PetGame/PetData.swift
@@ -15,9 +15,9 @@ class PetData{
     var lastPlayedWith : NSDate
     
     let hungerDuration : Double = 600.0 //Time to zero hunger in seconds
-    let washDuration : Double = 600.0 //Time to zero clean in seconds
-    let walkDuration : Double = 600.0 //Time to zero tired in seconds
-    let playDuration : Double = 600.0 //Time to zero play in seconds
+    let washDuration : Double = 600.0   //Time to zero clean in seconds
+    let walkDuration : Double = 600.0   //Time to zero tired in seconds
+    let playDuration : Double = 600.0   //Time to zero play in seconds
     
     init(){
         //Set all our values to the current date and time initially
@@ -25,6 +25,39 @@ class PetData{
         lastWashed = NSDate()
         lastWalked = NSDate()
         lastPlayedWith = NSDate()
+        
+        let lf = UserDefaults.standard.object(forKey: "LastFed")
+        let lws = UserDefaults.standard.object(forKey: "LastWashed")
+        let lwl = UserDefaults.standard.object(forKey: "LastWalked")
+        let lpw = UserDefaults.standard.object(forKey: "LastPlayedWith")
+        
+        if let date = lf as? NSDate{
+            lastFed = date
+        }
+        
+        if let date = lws as? NSDate{
+            lastWashed = date
+        }
+        
+        if let date = lwl as? NSDate{
+            lastWalked = date
+        }
+        
+        if let date = lpw as? NSDate{
+            lastPlayedWith = date
+        }
+    }
+    
+    deinit{
+        StoreData()
+    }
+    
+    func StoreData() -> Void{
+        let defaults = UserDefaults.standard
+        defaults.set(lastFed, forKey: "LastFed")
+        defaults.set(lastWashed, forKey: "LastWashed")
+        defaults.set(lastWalked, forKey: "LastWalked")
+        defaults.set(lastPlayedWith, forKey: "LastPlayedWith")
     }
     
     func GetHappiness() -> Double{
