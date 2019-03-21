dc9a4d5457bdafebde198122fa65dffd3adffb9c
Fukumoto Jared A <n01189530@nl-00115-015.mediastudies.ca>
2019-03-21 16:09:52 -0400

    Created Handler for Siri. XCode does not appear to allow the creation of needed files to continue.

---
+++ PetGame/PetGameSiri/PetGameSiriHandler.swift
@@ -0,0 +1,14 @@
+//
+//  PetGameSiriHandler.swift
+//  PetGameSiri
+//
+//  Created by Fukumoto Jared A. on 3/21/19.
+//  Copyright © 2019 Rennick Carter M. All rights reserved.
+//
+
+import Foundation
+import Intents
+
+class PetGameSiriHandler : NSObject{
+    
+}
