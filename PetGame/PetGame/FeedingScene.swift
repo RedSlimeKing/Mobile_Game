0d74a743c9360381943c6431923a2617c0ef47b3
Gallagher Sean D <n01163912@nl-00115-013.mediastudies.ca>
2019-03-28 17:53:25 -0400

    added new buttons for feeding mechanics

--- PetGame/PetGame/FeedingScene.swift
+++ PetGame/PetGame/FeedingScene.swift
@@ -13,7 +13,10 @@ class FeedingScene: BaseScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    
+    private var steakButton : SceneButton!
+    private var iceCreamButton : SceneButton!
+    private var dogFoodButton :SceneButton!
+    private var dog : SKSpriteNode?
     override func didMove(to view: SKView) {
         super.didMove(to: view)
         
@@ -22,10 +25,48 @@ class FeedingScene: BaseScene {
         background.position = CGPoint(x: size.width / 2, y: size.height / 2 )
         addChild(background)
         
+        
+     
         let testButton = SceneButton(text: "Back to Menu") {
             self.MoveToScene(newScene: Scenes.lobby)
         }
+        
+        steakButton = SceneButton(text: "Steak") {
+            print("dog really likes steak")
+            self.MoveButton()
+           
+        }
+        iceCreamButton = SceneButton(text: "Ice Cream") {
+            print("dog is sick now")
+            self.MoveButton()
+        }
+        dogFoodButton = SceneButton(text: "Dog food") {
+            print("dog eats dog food and enjoys it")
+            self.MoveButton()
+        }
+        
+        
         testButton.position = CGPoint(x: 100, y: -300 )
         background.addChild(testButton)
+        
+        dogFoodButton.position = CGPoint(x: 0, y: 300 )
+        steakButton.position = CGPoint(x: 0, y: 200 )
+        iceCreamButton.position = CGPoint(x: 0, y: 100 )
+        
+        background.addChild(steakButton)
+        background.addChild(iceCreamButton)
+        background.addChild(dogFoodButton)
     }
+    
+    
+    func MoveButton(){
+        
+        dogFoodButton.position = CGPoint(x: -50000000, y: -50000000 )
+        steakButton.position = CGPoint(x: -50000000, y: -50000000 )
+        iceCreamButton.position = CGPoint(x: -50000000, y: -50000000 )
+        
+    
+    }
+    
+    
 }
