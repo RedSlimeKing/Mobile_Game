a20babec70e98859a4ca88066cbfa59cfbb77447
Rennick Carter M <n01155242@nl-00115-016.mediastudies.ca>
2019-04-07 14:11:28 -0400

    Created new class for the Wash Scene's pet logic

---
+++ PetGame/PetGame/WashPet.swift
@@ -0,0 +1,31 @@
+//
+//  WashPet.swift
+//  PetGame
+//
+//  Created by Rennick Carter M. on 4/7/19.
+//  Copyright © 2019 Rennick Carter M. All rights reserved.
+//
+
+import Foundation
+
+import SpriteKit
+
+class WashPet : SKSpriteNode{
+    init(petTexture: String){
+        let texture = SKTexture.init(imageNamed: petTexture)
+        super.init(texture: texture, color: UIColor.clear, size: texture.size())
+        
+        self.isUserInteractionEnabled = true
+        self.size.width = self.size.width / 5.0
+        self.size.height = self.size.height / 5.0
+        self.zPosition = 10.0
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        super.touchesBegan(touches, with: event)
+    }
+}
