8b4ba0a4d5c43cc3f9cdbc93f33d25470bf90e0d
Rennick Carter M <n01155242@nl-00115-016.mediastudies.ca>
2019-04-11 16:47:23 -0400

    Updated wash scene to use new mechanics

--- PetGame/PetGame/Washing/WashScene.swift
+++ PetGame/PetGame/Washing/WashScene.swift
@@ -11,6 +11,10 @@ import GameplayKit
 
 class WashScene: BaseScene{
     var pet : WashPet? = nil
+    var progressBar : ProgressBar? = nil
+    let initialCleanValue : Double = 10.0
+    var cleanValue : Double = 0.0
+    var lastTime : Double = 0.0
     
     override func didMove(to view: SKView) {
         super.didMove(to: view)
@@ -20,31 +24,49 @@ class WashScene: BaseScene{
         background.position = CGPoint(x: size.width / 2, y: size.height / 2 )
         addChild(background)
         
+        let pbBackground = SKSpriteNode(imageNamed: "button")
+        pbBackground.size = CGSize(width: pbBackground.size.width / 3.0, height: pbBackground.size.height / 6.0)
+        pbBackground.position = CGPoint(x: 0.0, y: 200.0)
+        pbBackground.zPosition = 5.0
+        pbBackground.color = UIColor.darkGray
+        pbBackground.colorBlendFactor = 1.0
+        background.addChild(pbBackground)
+        
+        progressBar = ProgressBar(textureName: "button")
+        progressBar?.position = CGPoint(x: 0, y: 200)
+        progressBar?.SetFillValue(value: 0.0)
+        background.addChild(progressBar!)
+        
         let testButton = SceneButton(text: "Back to Menu") {
             self.MoveToScene(newScene: Scenes.lobby)
         }
         testButton.position = CGPoint(x: 100, y: -300 )
         background.addChild(testButton)
         
-        if let petObj = WashPet(petTexture: "button") as WashPet?{
+        if let petObj = WashPet(petTexture: "dognormalfood") as WashPet?{
             pet = petObj
             petObj.position = CGPoint(x: 0, y: 0)
+            petObj.size = CGSize(width: petObj.size.width * 1.5, height: petObj.size.height * 1.5)
             background.addChild(petObj)
         }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesMoved(touches, with: event)
+        
+        cleanValue = initialCleanValue
     }
     
     override func update(_ currentTime: TimeInterval){
+        let deltaTime : Double = currentTime - lastTime
+        lastTime = currentTime
+        
         if(pet?.IsBeingTouched())!{
+            cleanValue -= deltaTime
+            progressBar?.SetFillValue(value: CGFloat(1.0 - (cleanValue / initialCleanValue)))
+            if cleanValue <= 0.0 {
+                MoveToScene(newScene: Scenes.lobby)
+            }
+            
         }
         
+        pet?.update(currentTime)
         super.update(currentTime)
     }
 }
