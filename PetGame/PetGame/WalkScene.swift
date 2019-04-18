9ebec61c07524ce5210dd408dec0f2b49f87a4d8
Fukumoto Jared A <n01189530@nl-00115-015.mediastudies.ca>
2019-04-18 16:03:05 -0400

    Walk Scene complete barring Progress Bar Text which is not yet functioning

--- PetGame/PetGame/WalkScene.swift
+++ PetGame/PetGame/WalkScene.swift
@@ -13,15 +13,28 @@ class WalkScene: BaseScene{
     
     var distance : Double = 0.0
     var lastTime : Double = 0.0
+    var progressBar : ProgressBar? = nil
     
     override func didMove(to view: SKView) {
         super.didMove(to: view)
-        
         
         backgroundColor = SKColor.black
         let background = SKSpriteNode(imageNamed: "background1")
         background.position = CGPoint(x: size.width / 2, y: size.height / 2 )
         addChild(background)
+        
+        let pbBackground = SKSpriteNode(imageNamed: "button")
+        pbBackground.size = CGSize(width: pbBackground.size.width / 3.0, height: pbBackground.size.height / 6.0)
+        pbBackground.position = CGPoint(x: 0.0, y: 200.0)
+        pbBackground.zPosition = 5.0
+        pbBackground.color = UIColor.darkGray
+        pbBackground.colorBlendFactor = 1.0
+        background.addChild(pbBackground)
+        
+        progressBar = ProgressBar(textureName: "button", text: "Distance Walked: ")
+        progressBar?.position = CGPoint(x: 0, y: 200)
+        progressBar?.SetFillValue(value: 0.0)
+        background.addChild(progressBar!)
         
         let dogButton = SpriteButton(spriteName: "dognormalfood.png"){
             
@@ -29,7 +42,6 @@ class WalkScene: BaseScene{
         dogButton.position = CGPoint(x: 0, y: 0)
         dogButton.size = CGSize(width: dogButton.size.width * 2.0, height: dogButton.size.height * 2.0)
         background.addChild(dogButton)
-        
         
         let backToMenuButton = SceneButton(text: "Back to Menu") {
             self.MoveToScene(newScene: Scenes.lobby)
@@ -50,6 +62,8 @@ class WalkScene: BaseScene{
         if(distance > 500.0 ){
             PetData.instance.Walk()
         }
+        progressBar?.label.text = "Distance Walked: " + String(distance) + " / " + String(500.0)
+        progressBar?.SetFillValue(value: CGFloat(distance / 500.0))
         print(distance)
     }
 }
